86adef78bf78e5c367d3c0b550dbb45e70d0ec42
Tatiana <tatum@rambler.ru>
2022-08-19 23:40:54 +0300

    Task 027

---
+++ Task027/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+int SumNumber(int a)
+{
+    int result = 0;
+    while (a > 0)
+    {
+        result += a % 10;
+        a = a / 10;
+    }
+    return result;
+}
+Console.WriteLine(SumNumber(452));
+Console.WriteLine(SumNumber(82));
+Console.WriteLine(SumNumber(9012));
